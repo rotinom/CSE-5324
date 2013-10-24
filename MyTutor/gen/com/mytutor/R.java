b21eb63097c54d78bd63ecda3d832067e8f3836b
Dave Weber <rotinom-at-gmail-dot-com>
2013-10-24 18:50:48 -0500

    Cleaning up .gitignore

--- MyTutor/gen/com/mytutor/R.java
+++ MyTutor/gen/com/mytutor/R.java
@@ -28,14 +28,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int mytutor_icon=0x7f020001;
+        public static final int mytutor_logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f0a000a;
-        public static final int action_settings=0x7f0a000b;
-        public static final int button1=0x7f0a0007;
-        public static final int button2=0x7f0a0008;
-        public static final int button3=0x7f0a0009;
+        public static final int LinearLayout1=0x7f0a0007;
+        public static final int action_forgot_password=0x7f0a000c;
+        public static final int action_settings=0x7f0a000d;
+        public static final int button1=0x7f0a0008;
+        public static final int button2=0x7f0a0009;
+        public static final int button3=0x7f0a000a;
         public static final int email=0x7f0a0003;
+        public static final int imgLogo=0x7f0a000b;
         public static final int login=0x7f0a0005;
         public static final int login_form=0x7f0a0002;
         public static final int login_status=0x7f0a0000;
@@ -46,6 +50,7 @@ public final class R {
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_splash=0x7f030002;
     }
     public static final class menu {
         public static final int login=0x7f090000;
