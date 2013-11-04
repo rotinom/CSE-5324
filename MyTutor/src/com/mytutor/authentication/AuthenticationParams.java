9cea336489aa195747f22e134984101ffef14ac2
Dave Weber <rotinom-at-gmail-dot-com>
2013-11-03 23:17:22 -0600

    Back to where I was (almost)

--- MyTutor/src/com/mytutor/authentication/AuthenticationParams.java
+++ MyTutor/src/com/mytutor/authentication/AuthenticationParams.java
@@ -5,7 +5,7 @@ public class AuthenticationParams {
     /**
      * Account type id
      */
-    public static final String ACCOUNT_TYPE = "com.udinic.auth_example";
+    public static final String ACCOUNT_TYPE = "com.mytutor";
 
     /**
      * Account name
@@ -16,10 +16,10 @@ public class AuthenticationParams {
      * Auth token types
      */
     public static final String AUTHTOKEN_TYPE_READ_ONLY = "Read only";
-    public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an Udinic account";
+    public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to a MyTutor account";
 
     public static final String AUTHTOKEN_TYPE_FULL_ACCESS = "Full access";
-    public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an Udinic account";
+    public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to a MyTutor account";
 
 //    public static final ServerAuthenticate sServerAuthenticate = new ParseComServerAuthenticate();
 }
