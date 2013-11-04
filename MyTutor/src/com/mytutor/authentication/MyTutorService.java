c9983a684d0b8344eca49462daa41ae0700b0896
Dave Weber <rotinom-at-gmail-dot-com>
2013-11-03 22:38:14 -0600

    some more stuff.

---
+++ MyTutor/src/com/mytutor/authentication/MyTutorService.java
@@ -0,0 +1,15 @@
+package com.mytutor.authentication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyTutorService extends Service {
+
+	@Override
+    public IBinder onBind(Intent intent) {
+        MyTutorAuthenticator authenticator = new MyTutorAuthenticator(this);
+        return authenticator.getIBinder();
+    }
+
+}
