f2126b6985ebba7f85757d74ae368b2e8e241990
joneason <jonathan.w.eason@gmail.com>
2013-11-03 21:33:49 -0600

    Got search working, yay me
    Got search working, yay me. Still need to get listview working
    completely

--- MyTutor/src/com/mytutor/search/SearchParams.java
+++ MyTutor/src/com/mytutor/search/SearchParams.java
@@ -1,15 +1,26 @@
 package com.mytutor.search;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -17,6 +28,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.mytutor.R;
@@ -26,15 +39,24 @@ import com.mytutor.search.CustomHttpClient;
 public class SearchParams extends Activity
 {
 
+   private Map<String,String> subCatLookup_;
+ 
    @Override
    protected void onCreate(Bundle savedInstanceState) 
    {
+	   subCatLookup_ = new HashMap<String,String>();
+	   
 	   //allow network requests in main thread
 	   StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 	   StrictMode.setThreadPolicy(policy); 
 	   
 	   super.onCreate(savedInstanceState);
 	   setContentView(R.layout.activity_search);
+	   
+	   final Spinner mainCatSpinner_ = (Spinner)  findViewById(R.id.mainCatSpinner);
+	   final Spinner subCatSpinner_  = (Spinner)  findViewById(R.id.subCatSpinner);
+	   final Spinner radiusSpinner_  = (Spinner)  findViewById(R.id.searchRadius);
+	   final EditText zipcodeEdit_   = (EditText) findViewById(R.id.editZipcode);
 
 	   //create empty post parameters for php request
 	   ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
@@ -61,17 +83,16 @@ public class SearchParams extends Activity
 			   }
 
 			   //populate spinner for main categories
-			   final Spinner mainSpinner = (Spinner) findViewById(R.id.mainCatSpinner);
 			   ArrayAdapter<String> adp1=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mainCategories);
 			   adp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			   mainSpinner.setAdapter(adp1);	
+			   mainCatSpinner_.setAdapter(adp1);	
 			   //set up listener to handle main category selection (to populate sub-category spinner)
-			   mainSpinner.setOnItemSelectedListener(new OnItemSelectedListener() 
+			   mainCatSpinner_.setOnItemSelectedListener(new OnItemSelectedListener() 
 			   {
 			       @Override
 			       public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) 
 			       {
-			    	   populateSubCategory(mainSpinner.getItemAtPosition(position).toString());
+			    	   populateSubCategory(mainCatSpinner_.getItemAtPosition(position).toString());
 			       }
 
 			       @Override
@@ -87,15 +108,81 @@ public class SearchParams extends Activity
 	   catch (Exception e) {
 		   Log.e("log_tag","Error in http connection!!" + e.toString());     
 	   }
+	   
+       // Set up handler for the profile button on the bottom of the activity
+       final Button searchButton = (Button) findViewById(R.id.btnSubmit);
+
+       searchButton.setOnClickListener(
+   		  new View.OnClickListener()
+   		  {
+   			public void onClick(View v)
+   			{
+   		    	//Log.d("SearchActivity", "Got submit button click event");
+   				Intent intent = new Intent(v.getContext(), com.mytutor.search.SearchResults.class);
+   				
+   				String subCat = subCatSpinner_.getSelectedItem().toString();
+   				String radius = radiusSpinner_.getSelectedItem().toString();
+   				String zipcode = zipcodeEdit_.getText().toString();
+   				   				
+   				//turn zip into lat/lon
+   		    	//send request to google map api via http client
+   		        HttpGet httpGet = new HttpGet("http://maps.google.com/maps/api/geocode/json?address=" + zipcode +"&sensor=false");
+   		        HttpClient client = new DefaultHttpClient();
+   		        HttpResponse response;
+   		        StringBuilder stringBuilder = new StringBuilder();
+
+   		        //try and execute request and get response
+   		        try {
+   		            response = client.execute(httpGet);
+   		            HttpEntity entity = response.getEntity();
+   		            InputStream stream = entity.getContent();
+   		            int b;
+   		            while ((b = stream.read()) != -1) {
+   		                stringBuilder.append((char) b);
+   		            }
+   		        } catch (ClientProtocolException e) {
+   		            } catch (IOException e) {
+   		        }
+   		        
+   		        //Log.e("log_tag","Zipcode lookup: " + stringBuilder.toString());
+
+   		        String lat_string = "";
+   		        String lon_string = "";
+   		        //try parsing json encoded text
+   		        try {
+   		        	JSONObject request = new JSONObject(stringBuilder.toString());
+   		            JSONObject results = request.getJSONArray("results").getJSONObject(0);
+   		            JSONObject geometry = results.getJSONObject("geometry");
+   		            JSONObject location = geometry.getJSONObject("location");
+   		            
+   		            lat_string = location.getString("lat");
+   		            lon_string = location.getString("lng");
+   		            //Log.d("test", "lat: " + lat_string + " lon:" + lon_string);
+   		            
+   	   		        //populate intent with user data and launch results
+   	   				intent.putExtra("lat",lat_string);
+   	   				intent.putExtra("lon",lon_string);
+   	   				intent.putExtra("subcat",subCatLookup_.get(subCat));
+   	   				intent.putExtra("radius",radius);
+   	   				startActivity(intent);
+   		            
+   		        } catch (JSONException e) {
+   		            e.printStackTrace();
+   		        }
+   			}
+   		  }
+		); 
    }
    
    public void populateSubCategory(String mainCategory) {
-	   
+
+	   final Spinner subCatSpinner_  = (Spinner)  findViewById(R.id.subCatSpinner);
 	   //populate post parameter with main category selection
 	   ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
 	   postParameters.add(new BasicNameValuePair("category", mainCategory));
 	   
 	   String response = null;
+	   subCatLookup_.clear();
           
 	   //send off http request to php script on omega
 	   try {
@@ -114,11 +201,11 @@ public class SearchParams extends Activity
 				   //Log.i("log_tag","name: "+json_data.getString("name"));
 				   //Get an output to the screen
 				   mainCategories.add(json_data.getString("name"));
+				   subCatLookup_.put(json_data.getString("name"), json_data.getString("categoryId"));
 			   }
-			   final Spinner subSpinner = (Spinner) findViewById(R.id.subCatSpinner);
 			   ArrayAdapter<String> adp1=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mainCategories);
 			   adp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-			   subSpinner.setAdapter(adp1);			   
+			   subCatSpinner_.setAdapter(adp1);			   
 		   }
 		   catch(JSONException e){
 			   Log.e("log_tag", "Error parsing data "+e.toString());
--- MyTutor/src/com/mytutor/search/SearchResults.java
+++ MyTutor/src/com/mytutor/search/SearchResults.java
@@ -1,5 +1,68 @@
 package com.mytutor.search;
 
-public class SearchResults {
+import java.util.ArrayList;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mytutor.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class SearchResults extends Activity
+{
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) 
+   {
+	   super.onCreate(savedInstanceState);
+	   setContentView(R.layout.activity_results);
+	   
+	   //create empty post parameters for php request
+	   ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+	   postParameters.add(new BasicNameValuePair("subcat", getIntent().getExtras().getString("subcat")));
+	   postParameters.add(new BasicNameValuePair("radius", getIntent().getExtras().getString("radius")));
+	   postParameters.add(new BasicNameValuePair("lat", getIntent().getExtras().getString("lat")));
+	   postParameters.add(new BasicNameValuePair("lon", getIntent().getExtras().getString("lon")));
+	   String response = null;
+          
+       // call executeHttpPost method passing necessary parameters 
+	   try {
+		   //send off http request to php script on omega
+		   response = CustomHttpClient.executeHttpPost("http://omega.uta.edu/~jwe0053/search.php", postParameters);
+     
+		   // store the result returned by PHP script that runs MySQL query
+		   String result = response.toString();
+
+		   final ListView resultsView  = (ListView) findViewById(R.id.resultsView);
+		   ArrayList<String> list = new ArrayList<String>();
+              
+		   //parse json array data
+		   try{
+			   JSONArray jArray = new JSONArray(result);
+			   //Log.i("log_tag","data: "+ jArray.toString());
+			   for(int i=0;i<jArray.length();i++){
+				   JSONObject json_data = jArray.getJSONObject(i);
+				   //Log.i("log_tag","name: "+json_data.getString("firstName"));
+				   list.add(json_data.getString("firstName"));
+			   }
+			   resultsView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, list));
+		   
+		   }
+		   catch(JSONException e){
+			   Log.e("log_tag", "Error parsing data "+e.toString());
+		   }        
+	   }
+	   catch (Exception e) {
+		   Log.e("log_tag","Error in http connection!!" + e.toString());     
+	   }
+	   
+   }
 }
