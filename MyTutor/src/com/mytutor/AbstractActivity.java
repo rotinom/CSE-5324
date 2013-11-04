a1c3bede0618c0c2fc58a041379f4349e8477367
Dave Weber <rotinom-at-gmail-dot-com>
2013-11-03 22:46:25 -0600

    Add abstract activity

---
+++ MyTutor/src/com/mytutor/AbstractActivity.java
@@ -0,0 +1,13 @@
+package com.mytutor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public abstract class AbstractActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        
+    }
+}
