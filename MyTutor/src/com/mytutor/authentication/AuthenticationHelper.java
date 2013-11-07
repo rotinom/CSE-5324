577c2d8a35a5a5116e596b85a498bb274ce7a81a
Dave Weber <rotinom-at-gmail-dot-com>
2013-11-06 23:42:20 -0600

    First cut of authentication helper

---
+++ MyTutor/src/com/mytutor/authentication/AuthenticationHelper.java
@@ -0,0 +1,63 @@
+package com.mytutor.authentication;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerFuture;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mytutor.R;
+
+public class AuthenticationHelper {
+
+	private AccountManager accountManager_;
+	
+	Context context_;
+	
+	static private String log_name = "AuthenticationHelper";
+	
+	public AuthenticationHelper(Context context) {
+		// Set up our object
+		context_ = context;
+		accountManager_ = AccountManager.get(context_);
+	}
+	
+	public void remove_all_of_our_accounts(){
+		Account[] accounts = accountManager_.getAccounts();
+		Log.d(log_name, "Found " + accounts.length + " accounts");
+		
+		// If we have an account of our type, then return success
+		for(Account account : accounts){
+			Log.d(log_name, "Found account: " + account);
+			Log.d(log_name, AuthenticationParams.ACCOUNT_TYPE);
+			Log.d(log_name, account.type);
+			if(account.type.equals(AuthenticationParams.ACCOUNT_TYPE)){
+				Log.d(log_name, "Found account!!!: " + account);
+				AccountManagerFuture<Boolean> ret = accountManager_.removeAccount(account, null, null);
+				Log.d(log_name, "removeAccount returned: " + ret);
+				
+			}
+		}		
+	}
+	   
+	
+	public boolean has_account(){
+		Account[] accounts = accountManager_.getAccounts();
+		
+		// If we have an account of our type, then return success
+		for(Account account : accounts){
+			if(AuthenticationParams.ACCOUNT_TYPE == account.type){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String get_token(){
+		return "";
+	}
+	
+	
+	
+}
