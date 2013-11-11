5673a63be2bd980dcc308df2a47c9fe42b58b466
rfdutt <rfdutt@Richard-Dutts-MacBook-Pro.local>
2013-11-10 21:50:00 -0600

    Cleaned up splash screen code.

--- MyTutor/src/com/mytutor/SplashScreen.java
+++ MyTutor/src/com/mytutor/SplashScreen.java
@@ -3,7 +3,7 @@ package com.mytutor;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
  
 public class SplashScreen extends Activity {
  
@@ -14,27 +14,6 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
- 
-        //new Handler().postDelayed(new Runnable() {
- 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
- 
-            //@Override
-            //public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                //Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                //SplashScreen.this.startActivity(i);
- 
-                // close this activity
-                //SplashScreen.this.finish();
-            //}
-        //}, SPLASH_TIME_OUT);
-        
-        //Protocol.getInstance(this);
 
         Thread welcomeThread = new Thread() {
 
@@ -43,7 +22,7 @@ public class SplashScreen extends Activity {
                 try {
                     sleep(SPLASH_TIME_OUT);
                 } catch (Exception e) {
-                    //Log.e(getClass().getName(), e.toString());
+                    Log.e(getClass().getName(), e.toString());
                 } finally {
                     // This method will be executed once the timer is over
                     // Start your app main activity
