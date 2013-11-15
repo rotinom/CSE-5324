831d935aa58d0cec78b164ba604a4a43137d5c3c
Dave Weber <rotinom-at-gmail-dot-com>
2013-11-15 00:26:25 -0600

    Start profile class.  Handle ImageButton updates when rotations occur (would previously lose the image on the button)

---
+++ MyTutor/src/com/mytutor/Profile.java
@@ -0,0 +1,104 @@
+package com.mytutor;
+
+import java.io.ByteArrayOutputStream;
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+
+
+public class Profile {
+    private Bitmap picture_;
+    private String firstName_;
+    private String lastName_;
+    private String[] address_;
+    private String city_;
+    private String state_;
+    private String zipCode_;
+    private String[] subjects_;
+    
+    public Bitmap getPicture() {
+        return picture_;
+    }
+
+    public void setPicture(Bitmap picture) {
+        picture_ = picture;
+    }
+
+    public String getFirstName() {
+        return firstName_;
+    }
+
+    public void setFirstName(String firstName) {
+        firstName_ = firstName;
+    }
+
+    public String getLastName() {
+        return lastName_;
+    }
+
+    public void setLastName(String lastName) {
+        lastName_ = lastName;
+    }
+
+    public String[] getAddress() {
+        return address_;
+    }
+
+    public void setAddress(String[] address) {
+        address_ = address;
+    }
+
+    public String getCity() {
+        return city_;
+    }
+
+    public void setCity(String city) {
+        city_ = city;
+    }
+
+    public String getState() {
+        return state_;
+    }
+
+    public void setState(String state) {
+        state_ = state;
+    }
+
+    public String getZipCode() {
+        return zipCode_;
+    }
+
+    public void setZipCode(String zipCode) {
+        zipCode_ = zipCode;
+    }
+
+    public String[] getSubjects() {
+        return subjects_;
+    }
+
+    public void setSubjects(String[] subjects) {
+        subjects_ = subjects;
+    }
+
+    public String serialize() {
+        String ret = new String();
+        
+        
+
+        
+        
+        // Encode the bitmap into Base64
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();  
+        picture_.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] image = stream.toByteArray();
+        ret += Base64.encodeToString(image, Base64.DEFAULT);
+        
+        
+        return ret;
+    }
+    
+    public void deserialize(String data) {
+        
+    }
+}
--- MyTutor/src/com/mytutor/ProfileActivity.java
+++ MyTutor/src/com/mytutor/ProfileActivity.java
@@ -1,10 +1,14 @@
 package com.mytutor;
 
+import java.io.ByteArrayOutputStream;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -17,6 +21,8 @@ public class ProfileActivity extends Activity {
     private final static int PICK_FROM_CAMERA = 0;
 
     private final static String log_name = "ProfileActivity";
+    
+    private Bitmap photo_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +55,67 @@ public class ProfileActivity extends Activity {
             return;
         }
         
-        ImageButton profilePic = (ImageButton)findViewById(R.id.button_profile_pic);
-        Bitmap photo = (Bitmap) data.getExtras().get("data");
-        profilePic.setImageBitmap(photo);
         
-
+        
+        ImageButton profileButton = (ImageButton)findViewById(R.id.button_profile_pic);
+        photo_ = (Bitmap) data.getExtras().get("data");
+        
+        
+        
+        // Rescale the bitmap
+        int height = photo_.getHeight();
+        int width  = photo_.getWidth();
+        
+        int newHeight;
+        int newWidth;
+        if(height > width) {
+            Log.d(log_name, "Portrait");
+            newHeight = profileButton.getMeasuredHeight();
+            newWidth = (int)((double)newHeight * ((double)width / (double)height));
+        }
+        else {
+            newWidth = profileButton.getMeasuredWidth();
+            newHeight = (int)((double)newWidth * ((double)height / (double)width));
+        }
+        photo_ = Bitmap.createScaledBitmap(photo_, newWidth, newHeight, false);
+        
+        
+        
+        profileButton.setImageBitmap(photo_);
+    }
+    
+    
+    
+    // Save the state when we rotate
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        
+     // Encode the bitmap into Base64
+        if(null != photo_) {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();  
+            photo_.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            byte[] image = stream.toByteArray();
+            String photoString = Base64.encodeToString(image, Base64.DEFAULT);
+            
+            outState.putString("photo", photoString);
+        }
     }
 
-
+    // Deserialize the state when we have redrawn after a rotate
+    @Override  
+    public void onRestoreInstanceState(Bundle savedInstanceState) {  
+        super.onRestoreInstanceState(savedInstanceState);
+        
+        if(savedInstanceState.containsKey("photo")) {
+        
+            String photoString = savedInstanceState.getString("photo");
+            byte[] image = Base64.decode(photoString, Base64.DEFAULT);
+            photo_ = BitmapFactory.decodeByteArray(image, 0, image.length);
+            
+            ImageButton profileButton = (ImageButton)findViewById(R.id.button_profile_pic);
+            profileButton.setImageBitmap(photo_);
+        }
+        
+    }
 }
