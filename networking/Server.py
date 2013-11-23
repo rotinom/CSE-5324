3e7e38979d7610e584cffbb9f3bccec5eae8fe63
Dave Weber <rotinom-at-gmail-dot-com>
2013-11-23 12:46:08 -0600

    Updates to normalize code.  Some debugging

--- networking/Server.py
+++ networking/Server.py
@@ -1,25 +1,29 @@
+#!/usr/bin/python
+
 import sys, socket
 
 from ServerWorker import ServerWorker
 
-class Server:	
-	
-	def main(self):
-		try:
-			SERVER_PORT = int(sys.argv[1])
-		except:
-			print "[Usage: Server.py Server_port]\n"
-		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		rtspSocket.bind(('', SERVER_PORT))
-		rtspSocket.listen(5)        
+class Server:   
+    
+    def main(self):
+        try:
+            SERVER_PORT = int(sys.argv[1])
+        except:
+            print "[Usage: Server.py Server_port]\n"
+        rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print dir(socket)
+        rtspSocket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 16384)
+        rtspSocket.bind(('', SERVER_PORT))
+        rtspSocket.listen(5)        
 
-		# Receive client info (address,port) through RTSP/TCP session
-		while True:
-			clientInfo = {}
-			clientInfo['rtspSocket'] = rtspSocket.accept()
-			ServerWorker(clientInfo).run()		
+        # Receive client info (address,port) through RTSP/TCP session
+        while True:
+            clientInfo = {}
+            clientInfo['rtspSocket'] = rtspSocket.accept()
+            ServerWorker(clientInfo).run()      
 
 if __name__ == "__main__":
-	(Server()).main()
+    (Server()).main()
 
 
