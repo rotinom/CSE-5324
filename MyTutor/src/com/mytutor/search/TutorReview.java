5d61116cee8f9de562613b0738a18ca21129262d
joneason <joneason@macbook-pro.att.net>
2013-11-30 14:19:55 -0600

    Added rating popup to search profile
    Added rating popup to search profile

---
+++ MyTutor/src/com/mytutor/search/TutorReview.java
@@ -0,0 +1,67 @@
+package com.mytutor.search;
+
+import com.mytutor.R;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class TutorReview {
+   	public TutorReview()
+   	{
+   		
+   	}
+    
+    public static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
+    	final Dialog dialog = new Dialog(mContext);
+        dialog.setTitle("Rate " + "tutorName");
+        
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        
+        View v = inflater.inflate(R.layout.rating_popup, null);
+        View innerView = v.findViewById(R.id.LinearLayout);
+        
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setView(innerView);
+        final AlertDialog alertDialog = builder.create();
+        final RatingBar ratingBar = (RatingBar) v.findViewById(R.id.rating);
+    	final TextView ratingView = (TextView) v.findViewById(R.id.ratingLabel);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            	ratingView.setText(String.format("(%.1f)", rating));
+            	ratingBar.setRating(rating);
+        }});
+
+        final Button submitButton = (Button) v.findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				//submit review to db
+				alertDialog.dismiss();
+				
+				//todo submit rating to database with comment
+			}
+        });
+
+        final Button cancelButton = (Button) v.findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	alertDialog.dismiss();
+            }
+        }); 
+        alertDialog.show();
+    }
+}
--- MyTutor/src/com/mytutor/search/ViewProfile.java
+++ MyTutor/src/com/mytutor/search/ViewProfile.java
@@ -1,10 +1,14 @@
 package com.mytutor.search;
 import android.app.Activity;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import com.mytutor.search.TutorReview;
 
 import com.mytutor.R;
 
@@ -23,6 +27,7 @@ public class ViewProfile extends Activity
        TextView lastName = (TextView)findViewById(R.id.lastName);
        TextView rate = (TextView)findViewById(R.id.rate);
        RatingBar ratingBar = (RatingBar)findViewById(R.id.rating);
+       TextView ratingView = (TextView) findViewById(R.id.ratingInfo);
        TextView schedule = (TextView)findViewById(R.id.schedule);
        TextView profile = (TextView)findViewById(R.id.comment);
        
@@ -35,8 +40,16 @@ public class ViewProfile extends Activity
        lastName.setText(getIntent().getExtras().getString("lastname"));
        rate.setText(getIntent().getExtras().getString("rate"));
        ratingBar.setRating(Float.parseFloat(getIntent().getExtras().getString("rating")));
+       ratingView.setText(String.format("(%.1f)", Float.parseFloat(getIntent().getExtras().getString("rating"))));
        schedule.setText(getIntent().getExtras().getString("schedule"));
        profile.setText(getIntent().getExtras().getString("profile"));
        Log.i("SearchResults", "rating=" + getIntent().getExtras().getString("rating"));
+   }
+   
+   /** Called when the user touches the button */
+   public void rateMeClicked(View view) {
+       Editor editor = null;
+	// Do something in response to button click
+	   TutorReview.showRateDialog(this, editor);
    }
 }
